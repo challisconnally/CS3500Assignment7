97bebf97656a2095ca29f209f721b74ca699600c
Erik Hilton <u1335354@umail.utah.edu>
2025-03-21 11:28:23 -0600

    changed password form submission

--- LibraryWebServer/Models/Team14LibraryContext.cs
+++ LibraryWebServer/Models/Team14LibraryContext.cs
@@ -27,7 +27,7 @@ public partial class Team14LibraryContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=atr.eng.utah.edu;user id=u1335354;password=ScholarOfKopta25;database=Team14Library", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.11.8-mariadb"));
+        => optionsBuilder.UseMySql("server=atr.eng.utah.edu;user id=u1335354;password=pwd;database=Team14Library", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.11.8-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
